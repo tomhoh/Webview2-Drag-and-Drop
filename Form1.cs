134cf793db00343dcff710fa9a81c2318b028ab4
tomhoh <tomhoh@mac.com>
2022-03-29 18:09:23 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Webview_Drag_and_Drop
+{
+	public partial class Form1 : Form
+	{
+
+		public WebView2 wv2;
+
+		public Form1()
+		{
+			InitializeComponent();
+			Init();
+		}
+
+		public async void Init()
+		{
+			wv2 = new WebView2();
+			this.Controls.Add(wv2);
+			wv2.Dock = DockStyle.Fill;
+			await wv2.EnsureCoreWebView2Async();
+
+			wv2.CoreWebView2.AddHostObjectToScript("WV_File", new WV_File(this));
+
+			string path = "file:///" + Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "index.html");
+			wv2.Source = new Uri(path);
+		}
+	}
+
+	[ComVisible(true)]
+	public class WV_File
+	{
+
+		Form1 M;
+
+		public WV_File(Form1 m)
+		{
+			this.M = m;
+		}
+
+		public void DragDropFile(string path)
+		{
+			Console.WriteLine("draganddrop");
+            if (File.Exists(path) == false)
+            {
+                Console.WriteLine("file not exists");
+                return;
+            }
+            string[] files = { path };
+			try
+			{
+				Console.WriteLine("DragTry");
+				var file = new System.Windows.Forms.DataObject(System.Windows.Forms.DataFormats.FileDrop, files);
+				M.DoDragDrop(file, DragDropEffects.All);
+			}
+			catch { }
+		}
+	}
+}
---
+++ Form2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Webview_Drag_and_Drop
+{
+	public partial class Form2 : Form
+	{
+
+		public WebView2 wv3;
+
+		public Form2()
+		{
+			InitializeComponent();
+			Init();
+		}
+		public async void Init()
+		{
+			wv3 = new WebView2();
+			this.Controls.Add(wv3);
+			wv3.Dock = DockStyle.Fill;
+			await wv3.EnsureCoreWebView2Async();
+
+			string path = "file:///" + Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "page1.html");
+			wv3.Source = new Uri(path);
+
+			wv3.CoreWebView2.NewWindowRequested += (sender2, e2) => {
+				String _fileurl = e2.Uri.ToString();
+				e2.Handled = true;
+				RunJs($"var temp_dropPath = \"{_fileurl}\"");
+			};
+		}
+
+		public void RunJs(String js)
+		{
+			if (wv3.CoreWebView2 != null)
+				wv3.CoreWebView2.ExecuteScriptAsync(js);
+		}
+	}
+
+}
